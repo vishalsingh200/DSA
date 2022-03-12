eda3f1d1326aeb4d3ad0667849d43e8c608073c4
Vishal Kumar Singh <singhvishal0847@gmail.com>
2022-03-13 02:57:00 +0530

    Initial Commit

---
+++ src/Bubble_Sort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int temp;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Number of element enter:");
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+
+        }
+        for (int i = 0; i<arr.length-1; i++){
+            for (int j = 0; j<arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]){  //for incresing order
+
+                    //if (arr[j] < arr[j+1])
+                    // for decreasing order
+
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i<arr.length ; i++){
+            System.out.println("Elements are:"+arr[i]);
+        }
+    }
+}
