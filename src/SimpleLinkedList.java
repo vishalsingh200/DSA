4a1553e40ff4c5885c34d214c461bda77fe069c1
Vishal Kumar Singh <singhvishal0847@gmail.com>
2022-03-17 20:10:00 +0530

    Initial Commit

---
+++ src/SimpleLinkedList.java
@@ -0,0 +1,27 @@
+//Simple Linked List
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+public class SimpleLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        printList(head);
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr!=null){
+            System.out.println(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
